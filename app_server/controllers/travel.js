c63ba0df9bac2d740317aa67ea1e77a807972d68
Liam McAveney <liam.mcaveney@snhu.edu>
2025-07-14 08:23:36 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.redner('travel',{ title: 'Tarvlr Getaways'});
+    res.redner('travel',{ title: 'Tarvlr Getaways', trips});
 };
 
 module.exports = {
